82faac86dfcdbaf4051d39557db48281e0ef5651
nitudavid <74198733+nitudavid@users.noreply.github.com>
2023-03-11 13:04:00 +0200

    Delete Laboratoare APD directory

--- Laboratoare APD/lab02/test_mutex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f "mutex" ]
-then
-    echo "Nu exista binarul mutex"
-    exit
-fi
-
-for i in {1..1000}; do ./mutex; done
--- Laboratoare APD/lab04/src/bug6/MyThread.java
+++
@@ -1,8 +0,0 @@
-package bug6;
-
-public class MyThread extends Thread {
-    @Override
-    public void run() {
-        Singleton.getInstance();
-    }
-}
